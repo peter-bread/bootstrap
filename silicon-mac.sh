cdc16ec588c02ca95fd649a56b4930112b705b00
Peter Sheehan <petersheehan2003@gmail.com>
2024-09-30 17:45:47 +0100

    refactor: double quote full filepath

--- silicon-mac.sh
+++ silicon-mac.sh
@@ -343,7 +343,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs |
 
 source "$HOME/.cargo/env"
 
-rustup completions zsh >"$ZSH_COMPLETIONS"/_rustup
+rustup completions zsh >"$ZSH_COMPLETIONS/_rustup"
 
 # neovim - - - - - - - - - - - - - - - - - - - - - - - - - -
 
